9fcd60149bfc0950b96d4125002d47255065bac8
Ana Rita Poças <73423901+anaritaasp@users.noreply.github.com>
2023-04-18 11:32:06 +0100

    added assignment examples

---
+++ Ficha Análise Sintática (Yacc)/ex_calc_yacc.py
@@ -0,0 +1,57 @@
+import ply.yacc as yacc
+
+from calc_lex import tokens
+
+"""
+expression : expression PLUS term
+expression : expression MINUS term
+expression : term
+
+term : term TIMES factor
+term : term DIVIDE factor
+term : factor
+
+factor : NUMBER
+factor : LPAREN expression RPAREN
+"""
+
+def p_expression_plus(p):
+    'expression : expression PLUS term'
+    p[0] = p[1] + p[3]
+
+def p_expression_minus(p):
+    'expression : expression MINUS term'
+    p[0] = p[1] - p[3]
+
+def p_expression_term(p):
+    'expression : term'
+    p[0] = p[1]
+
+def p_term_times(p):
+    'term : term TIMES factor'
+    p[0] = p[1] * p[3]
+
+def p_term_div(p):
+    'term : term DIVIDE factor'
+    p[0] = p[1] / p[3]
+
+def p_term_factor(p):
+    'term : factor'
+    p[0] = p[1]
+
+def p_factor_num(p):
+    'factor : NUMBER'
+    p[0] = p[1]
+
+def p_factor_expr(p):
+    'factor : LPAREN expression RPAREN'
+    p[0] = p[2]
+
+def p_error(p):
+    print("Erro sintático no input!")
+
+parser = yacc.yacc()
+
+while s := input('calc > '):
+   result = parser.parse(s)
+   print(result)
